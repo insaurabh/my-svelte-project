abaae63e31eb5db348c6acbb800d4698bcafeabf
Saurabh Ranjan <inrsaurabh@gmail.com>
2022-10-22 17:22:05 +0530

    Init

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let count: number = 0
+  $: doubled = count *2
+
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment} > 
+ Clicked  { count } { count === 1 ? 'time' : 'times'}
+</button>
+
+<p> {count} doubled is {doubled} </p>
